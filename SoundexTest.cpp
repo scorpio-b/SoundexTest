05a447dfc1cbe45f4c7ca143635e563517381dde
scorpio-b <scorpio_b@126.com>
2025-06-01 23:37:07 +0800

    添加测试用例

---
+++ SoundexTest.cpp
@@ -0,0 +1,21 @@
+//
+// Created by scorpio-b on 5/31/25.
+//
+
+#include "gmock/gmock.h"
+#include <string>
+using ::testing::Eq;
+
+class Soundex {
+public:
+    std::string encode(const std::string& word) const
+    {
+        return "A";
+    }
+};
+
+TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
+    Soundex soundex;
+    auto encoded = soundex.encode("A");
+    ASSERT_THAT(encoded, Eq("A"));
+}
