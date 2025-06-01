40321c05321f16aa78c2692afe28ce546f8487bc
scorpio-b <scorpio_b@126.com>
2025-06-02 00:31:03 +0800

    重构测试用例并做了精简

--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -4,39 +4,32 @@
 
 #include "gmock/gmock.h"
 #include <string>
-using ::testing::Eq;
+using namespace testing;
 
-class Soundex
-{
+class Soundex {
 public:
-    std::string encode(const std::string& word) const
+    [[nodiscard]] std::string encode(const std::string& word) const
     {
         return zeroPad(word);
     }
 
 private:
-    std::string zeroPad(const std::string& word) const
+    [[nodiscard]] std::string zeroPad(const std::string& word) const
     {
         return word + "000";
     }
 };
 
-class SoundexEncoding : public ::testing::Test
+class SoundexEncoding : public Test
 {
 public:
     Soundex soundex;
 };
 
-TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord)
-{
-    auto encoded = soundex.encode("A");
-
-    ASSERT_THAT(encoded, Eq("A000"));
+TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
+    ASSERT_THAT(soundex.encode("A"), Eq("A000"));
 }
 
-TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits)
-{
-    auto encoded = soundex.encode("I");
-
-    ASSERT_THAT(encoded, Eq("I000"));
+TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits) {
+    ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
