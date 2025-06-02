38c3f62455d6d308ee494740148145a94db6abe8
scorpio-b <scorpio_b@126.com>
2025-06-02 13:43:10 +0800

    完善运行函数

--- Soundex.h
+++ Soundex.h
@@ -7,19 +7,50 @@
 
 #include <string>
 
-class Soundex {
+static const size_t MaxCodeLength{4};
+
+class Soundex
+{
 public:
-    [[nodiscard]] std::string encode(const std::string& word) const {
-        if (word == "Ab") {
-            return "A100";
-        }
-        return zeroPad(word);
+    [[nodiscard]] static std::string encode(const std::string& word)
+    {
+        return zeroPad(head(word) + encodedDigits(word));
     }
 
 private:
-    [[nodiscard]] std::string zeroPad(const std::string& word) const {
-        auto zeroNeeded = 4 - word.length();
+    [[nodiscard]] static std::string zeroPad(const std::string& word)
+    {
+        auto zeroNeeded = MaxCodeLength - word.length();
         return word + std::string(zeroNeeded, '0');
+    }
+
+    [[nodiscard]] static std::string head(const std::string& word)
+    {
+        return word.substr(0, 1);
+    }
+
+    [[nodiscard]] static std::string encodedDigits(const std::string& word)
+    {
+        if (word.length() > 1)
+        {
+            return encodedDigit(word[1]);
+        }
+
+        return "";
+    }
+
+    static std::string encodedDigit(char letter)
+    {
+        const std::unordered_map<char, std::string> encodings {
+            {'b', "1"}, {'f', "1"}, {'p', "1"}, {'v', "1"},
+            {'c', "2"}, {'g', "2"}, {'j', "2"}, {'k', "2"}, {'q', "2"}, {'s', "2"}, {'x', "2"}, {'z', "2"},
+            {'d', "3"}, {'t', "3"},
+            {'l', "4"},
+            {'m', "5"}, {'n', "5"},
+            {'r', "6"},
+        };
+        auto it = encodings.find(letter);
+        return it == encodings.end() ? "" : it->second;
     }
 };
 
--- SoundexTest.cpp
+++ SoundexTest.cpp
@@ -12,11 +12,16 @@ public:
     Soundex soundex;
 };
 
-TEST_F(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
-    ASSERT_THAT(soundex.encode("Ab"), Eq("A100"));
+TEST_F(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits)
+{
+    EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
+    EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
+    EXPECT_THAT(soundex.encode("Ad"), Eq("A300"));
+    EXPECT_THAT(soundex.encode("Ax"), Eq("A200"));
+    EXPECT_THAT(soundex.encode("A#"), Eq("A000"));
 }
 
-TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits) {
+TEST_F(SoundexEncoding, PadsWithZeroToEnsureThreeDigits)
+{
     ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
-
